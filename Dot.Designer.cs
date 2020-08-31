883785d586b07ae8170a33446d96afaff9bb4857
Daniel Stoughton <Daniel.Stoughton@gmail.com>
2020-08-31 16:32:50 -0500

    Added the project.

---
+++ Dot.Designer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Temp
+{
+    partial class DotForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            this.WindowState = FormWindowState.Normal;
+            this.Location = new Point(0, 0);
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Bounds = Screen.PrimaryScreen.WorkingArea;
+            this.BackColor = System.Drawing.Color.FromArgb(0, 255, 0);
+            this.TransparencyKey = System.Drawing.Color.FromArgb(0, 255, 0);
+            //this.DoubleBuffered = true;
+            this.Text = "The Dot";
+        }
+    }
+}
---
+++ Dot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Temp
+{
+    public partial class DotForm : Form
+    {
+        private int dotSize = 10;
+
+        public DotForm()
+        {
+            InitializeComponent();
+
+            var initialStyle = GetWindowLong(this.Handle, -20);
+            SetWindowLong(this.Handle, -20, initialStyle | 0x80000 | 0x20);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            using var myBrush = new SolidBrush(Color.Gray);
+            using var formGraphics = this.CreateGraphics();
+
+            formGraphics.FillEllipse(myBrush, new Rectangle(Screen.PrimaryScreen.Bounds.Width / 2 - dotSize, Screen.PrimaryScreen.Bounds.Height / 2 - dotSize, dotSize, dotSize));
+        }
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+    }
+}
